98c147bf423a5288455d83a0489110b55878cdb2
Richard Zhang <richard_zhang12@yahoo.com>
2021-06-15 05:28:35 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home' })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' })
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact' })
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
