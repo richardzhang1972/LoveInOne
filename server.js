cbb79749fcb7a0f53c72e17375b691acaf1d339f
Richard Zhang <richard_zhang12@yahoo.com>
2021-06-16 05:55:40 -0500

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
-const port = 3000
+
+const port = process.env.PORT || 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
